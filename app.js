7521951bc231d665ad5436114dd2b6e9e06dadff
gittalha456 <110527294+aquiltalha@users.noreply.github.com>
2023-09-16 15:06:12 +0530

    talking input

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log("Hello my name is Talha Aquil");
+ var btnTranslate=document.querySelector("#btn-translate");
+ var txtInput=document.querySelector("#txt-input");
+ console.log(txtInput);
 
+ function clickHandler(){
+   console.log("clicked");
+   console.log("input",txtInput.value);
+ };
+ 
+btnTranslate.addEventListener("click",clickHandler);
