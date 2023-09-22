0ec0b4065a62ec44b908911e7040d68bd738badf
gittalha456 <110527294+aquiltalha@users.noreply.github.com>
2023-09-22 11:39:05 +0530

    mark6-done

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
  var btnTranslate=document.querySelector("#btn-translate");
  var txtInput=document.querySelector("#txt-input");
+ var OutputDiv=document.querySelector("#output");
  console.log(txtInput);
+  
+ var serverURL=	"https://api.funtranslations.com/translate/minion.json"
 
+
+ function getTranslationURL(text){
+  return serverURL+ "?" +"text=" + text
+ }
+ function errorHandler(error)
+{
+  console.log("error occured",error);
+
+}
  function clickHandler(){
-   console.log("clicked");
-   console.log("input",txtInput.value);
- };
- 
+   //input
+    var inputText=txtInput.value;
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json=>{
+        var translatedText=json.contents.translated;
+        //output
+        OutputDiv.innerText=translatedText;
+      })
+  };
+  
 btnTranslate.addEventListener("click",clickHandler);
